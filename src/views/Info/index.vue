abb77cea64df77a021c47f52a297aa44cf6426c9
unknown <Administrator@WIN7-BV481MN8O3.trans-cosmos.com.cn>
2020-09-25 17:37:15 +0800

    制作信息管理页面

--- src/views/Info/index.vue
+++ src/views/Info/index.vue
@@ -1,16 +1,98 @@
 <template>
-    <div>信息列表</div>
+  <div>
+    <el-form :inline="true" class="demo-form-inline">
+        <el-row>
+            <el-col :span="4">
+                <el-form-item label="类型:">
+                    <el-select v-model="value" placeholder="请选择" style="width:120px">
+                        <el-option v-for="item in options"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value">
+                        </el-option>
+                    </el-select>
+                </el-form-item>
+            </el-col>
+            <el-col :span="8">
+                <el-form-item label="日期:">
+                    <el-date-picker
+                        v-model="value3"
+                        type="datetimerange"
+                        range-separator="至"
+                        start-placeholder="开始日期"
+                        end-placeholder="结束日期">
+                    </el-date-picker>
+                </el-form-item>
+            </el-col>
+            <el-col :span="3">
+                <el-form-item label="关键字:">
+                    <el-select v-model="search_key" style="width:80px">
+                        <el-option v-for="item in searchOptions"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value">
+                        </el-option>
+                    </el-select>
+                </el-form-item>
+            </el-col>
+            <el-col :span="4">
+                <el-input v-model="search_keyWord" placeholder="请输入内容"></el-input>
+            </el-col>
+        </el-row>
+    </el-form>
+  </div>
 </template>
 
 <script>
+import { reactive, ref } from '@vue/composition-api';
 export default {
-    name:"infoIndex",
-    setup(props,{root}){
+  name: "infoIndex",
+  setup(props, { root }) {
+    const options = reactive([{
+            value: '选项1',
+            label: '国际信息'
+        }, {
+            value: '选项2',
+            label: '国内信息'
+        }, {
+          value: '选项3',
+          label: '行业信息'
+    }]);
 
+
+    const searchOptions = reactive([
+        {value:'id',label:'ID'},
+        {value:'title',label:'标题'}
+    ])
+
+    const value = ref('');
+
+    const value3 = ref('');
+
+    const search_key = ref('id');
+
+    const search_keyWord = ref('');
+
+    return {
+        options,
+        value,
+        value3,
+        searchOptions,
+        search_key,
+        search_keyWord
     }
-}
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
+.box{
+    height: 20px;
+}
+.box-gray{
+    background-color: #ccc;
+}
+.box-red{
+    background-color: red;
+}
 </style>
