577e2c4570fa01490f26764ccd5733baa634739d
unknown <Administrator@WIN7-BV481MN8O3.trans-cosmos.com.cn>
2020-10-14 17:31:16 +0800

    制作插槽

--- src/components/Table/index.vue
+++ src/components/Table/index.vue
@@ -1,12 +1,20 @@
 <template>
      <el-table :data="data.tableData" style="width: 100%"  border>
         <el-table-column v-if="data.tableConfig.selection" type="selection" width="40"></el-table-column>
-        <el-table-column 
-            v-for="item in data.tableConfig.tHead" 
-            :key="item.field" 
-            :prop="item.field" 
-            :label="item.label"
-            :width="item.width"/>
+        <template v-for="item in data.tableConfig.tHead" >
+            <el-table-column :key="item.field" :prop="item.field" 
+                :label="item.label" v-if="item.columnType === 'slot'">
+                <template slot-scope="scope">
+                    <slot :name="item.slotName" :myData="scope.row"></slot>
+                </template>
+            </el-table-column>
+            <el-table-column
+                :key="item.field" 
+                :prop="item.field" 
+                :label="item.label"
+                :width="item.width"
+                v-else/>
+        </template>
     </el-table>
 </template>
 
--- src/views/User/index.vue
+++ src/views/User/index.vue
@@ -22,7 +22,15 @@
             </el-col>
         </el-row>
         <div style="padding-top:20px">
-            <TableVue :config.sync="data.configTable"/>
+            <TableVue :config.sync="data.configTable">
+                <template v-slot:status="slotData">
+                    <el-switch v-model="slotData.myData.name" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+                </template>
+                <template v-slot:operation="slotData">
+                    <el-button size="small" type="success" @click="edit(slotData.myData)">编辑</el-button>
+                    <el-button size="small" type="danger" @click="deleteUserInfo(slotData.myData)">删除</el-button>
+                </template>
+            </TableVue>
         </div>
     </div>
 </template>
@@ -49,12 +57,15 @@ export default {
                 // table header
                 recordCheckbox:true,
                 tHead:[
-                    { label:"邮箱/用户名称",field:"email",width:265},
-                    { label:"真实姓名",field:"name",width:220},
-                    { label:"手机号",field:"phone",width:260},
-                    { label:"地区",field:"address",width:500},
-                    { label:"角色",field:"role"}
-                ]
+                    { label:"邮箱/用户名称",field:"email",width:150},
+                    { label:"真实姓名",field:"name",width:100},
+                    { label:"手机号",field:"phone",width:150},
+                    { label:"地区",field:"address",width:300},
+                    { label:"角色",field:"role"},
+                    { label:"禁启用状态",field:"status",columnType:"slot",slotName:"status"},
+                    { label:"操作",columnType:"slot",slotName:"operation"}
+                ],
+                requestUrl:""
             }
         })
 
@@ -62,11 +73,21 @@ export default {
             // console.log(`+++++++++++${data.search_key}`);
         }
 
+        const edit = (data) => {
+            console.log(data);
+        }
+
+        const deleteUserInfo = (data) => {
+            console.log(data);
+        }
+
         return {
             // reactive
             data,
             // method
-            search
+            search,
+            edit,
+            deleteUserInfo
         }
     }
 }
@@ -78,3 +99,11 @@ export default {
     @include labelDom(left, 60, 65, 40);
 }
 </style>
+<!--
+v-slot插槽:父组件传值到子组件显示
+
+1.匿名插槽
+2.具名插槽：指定插槽显示内容
+3.作用域插槽：可以进行数据绑定，父子组件通讯
+
+-->
